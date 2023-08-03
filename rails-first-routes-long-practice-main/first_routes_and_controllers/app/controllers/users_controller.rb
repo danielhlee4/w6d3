f065aa6001356c3ca4b56acfab42d439dbbb0e56
Dennis Lee <dennislee1499@gmail.com>
2023-08-03 10:34:59 -0400

    finished update

--- rails-first-routes-long-practice-main/first_routes_and_controllers/app/controllers/users_controller.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create 
-        user = User.new(params.require(:user).permit(:name, :email))
+        user = User.new(user_params)
         if user.save
             render json: user
         else
@@ -14,7 +14,22 @@ class UsersController < ApplicationController
     end
 
     def show
-        user
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end

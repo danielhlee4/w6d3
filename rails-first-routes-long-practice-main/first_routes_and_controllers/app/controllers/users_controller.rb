d957cae205d02d4b4bd4afb1714746a99553adef
Dan Lee <danielhlee4@gmail.com>
2023-08-03 10:07:24 -0400

    add #create in users_controller

--- rails-first-routes-long-practice-main/first_routes_and_controllers/app/controllers/users_controller.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
-        render json: anything 
+        @users = User.all
+        render json: @users
     end
 
     def create 
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
+        user
         render json: params
     end
 end

a87591d68168e1efe91fcb9fdb493bd1d74ffa02
Dan Lee <danielhlee4@gmail.com>
2023-08-03 15:05:14 -0400

    add CreateArtworkShares migration

--- rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/artwork.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/artwork.rb
@@ -1,3 +1,7 @@
 class Artwork < ApplicationRecord
     validates :title, uniqueness: { scope: :artist_id }
+
+    belongs_to :artist,
+        foreign_key: :artist_id,
+        class_name: :User
 end
--- rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/user.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     validates :username, uniqueness: true, presence: true
+
+    has_many :artworks,
+        foreign_key: :artist_id,
+        class_name: :Artwork,
+        dependent: :destroy
 end

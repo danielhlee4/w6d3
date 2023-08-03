bfd82dab36b004f2298455749326f6a91db60cf4
Dennis Lee <dennislee1499@gmail.com>
2023-08-03 14:20:46 -0400

    created artworks table

--- rails-first-routes-long-practice-main/first_routes_and_controllers/db/seeds.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    User.destroy_all
+    User.create(username: 'Dan Lee')
+    User.create(username: 'Dennis Lee')
+    
+

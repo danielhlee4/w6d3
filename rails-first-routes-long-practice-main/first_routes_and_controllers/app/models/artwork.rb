df5d24470c6ddc3bff3750a33d1d0b2b9c91afc9
Dennis Lee <dennislee1499@gmail.com>
2023-08-03 15:32:04 -0400

    starting associations

--- rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/artwork.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/artwork.rb
@@ -4,4 +4,12 @@ class Artwork < ApplicationRecord
     belongs_to :artist,
         foreign_key: :artist_id,
         class_name: :User
+
+    has_many :shares,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkShare,
+    dependent: :destroy
+
+    has_many :shared_viewers,
+    through: :shares
 end
--- rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/user.rb
+++ rails-first-routes-long-practice-main/first_routes_and_controllers/app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
         foreign_key: :artist_id,
         class_name: :Artwork,
         dependent: :destroy
+
+    has_many :views,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare,
+    dependent: :destroy
+
+    has_many :shared_artworks,
+    through: :views,
+    source: :artwork
 end
